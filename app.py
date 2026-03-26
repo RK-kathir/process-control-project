8c3fd93148c337eb5141fd21eac2c82cc925b08b
RK-kathir <kvelkvp@gmail.com>
2026-03-27 00:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ import traceback
 app = Flask(__name__)
 CORS(app)
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-generation_config = {"response_mime_type": "application/json"}
-llm_model = genai.GenerativeModel("gemini-1.5-flash-latest", generation_config=generation_config)
-
+llm_model = genai.GenerativeModel("gemini-pro")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # =====================================================================
